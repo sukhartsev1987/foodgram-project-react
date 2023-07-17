06b46862c72cdee3f3bf4c4a0853a834453681d2
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-17 19:39:56 +0300

    readme

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,7 +6,7 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = (
         'first_name',
-        'last_name'
+        'last_name',
         'username',
         'email'
     )
