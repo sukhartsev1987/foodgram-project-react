5940269432160a9dcbae8453166486d9b3e10646
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-18 22:35:36 +0300

    readme

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
+    'colorfield',
     'drf_extra_fields.fields'
 ]
 
@@ -94,10 +95,16 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': 6,
 }
+
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
