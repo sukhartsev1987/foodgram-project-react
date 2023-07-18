e934081baa6995a51b7e8392ed326ce63b1421be
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-18 09:38:02 +0300

    readme

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from djoser.serializers import UserSerializer, UserCreateSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework.exceptions import ValidationError
+# from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
-from rest_framework import serializers, status
+from rest_framework import serializers
+# , status
 
 from users.models import User
 from recipes.models import (
