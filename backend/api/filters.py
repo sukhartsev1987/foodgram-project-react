af9822f866e0e82314295024794a06a8835991ee
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-19 09:45:11 +0300

    readme

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -34,7 +34,7 @@ class RecipeFilter(FilterSet):
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(shopping_list__user=self.request.user)
-        return queryset
+        return
 
 
 class IngredientFilter(SearchFilter):
