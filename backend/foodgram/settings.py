32a03aac14e48e8f5f7b00d6d63be535b2d7305a
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-19 20:56:20 +0300

    readme

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,10 +95,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': 6,
 }
 
 
