7b11d0325981cc6d00dfbd44ad470f71eb18e8e7
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-19 19:11:51 +0300

    readme

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,5 @@
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 
 from users.models import Follow, User
@@ -150,26 +148,6 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Время готовки должно быть не меньше одной минуты')
         return cooking_time
-
-    def validate_ingredients(self, value):
-        ingredients = value
-        if not ingredients:
-            raise ValidationError({
-                'ingredients': 'Нужен минимум один ингредиент!'
-            })
-        ingredients_list = []
-        for item in ingredients:
-            ingredient = get_object_or_404(Ingredient, id=item['id'])
-            if ingredient in ingredients_list:
-                raise ValidationError({
-                    'ingredients': 'Ингридиенты не могут повторяться!'
-                })
-            if int(item['amount']) <= 0:
-                raise ValidationError({
-                    'amount': 'Количество ингредиента должно быть больше 0!'
-                })
-            ingredients_list.append(ingredient)
-        return value
 
     @staticmethod
     def create_ingredients(recipe, ingredients):
