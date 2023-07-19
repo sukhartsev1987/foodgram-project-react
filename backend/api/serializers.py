bccf871e4ca9c5cdf0dbbb5185fdfa3830aa452a
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-19 19:39:29 +0300

    readme

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -100,9 +100,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 class IngredientRecipeSerializer(serializers.ModelSerializer):
     name = serializers.ReadOnlyField(source='ingredient.name')
-    id = serializers.PrimaryKeyRelatedField(
-        queryset=Ingredient.objects.all()
-    )
+    id = serializers.IntegerField(source='ingredient.id')
     measurement_unit = serializers.ReadOnlyField(
         source='ingredient.measurement_unit'
     )
@@ -149,18 +147,14 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
                 'Время готовки должно быть не меньше одной минуты')
         return cooking_time
 
-    @staticmethod
-    def create_ingredients(recipe, ingredients):
-        ingredient_liist = []
-        for ingredient_data in ingredients:
-            ingredient_liist.append(
-                IngredientRecipe(
-                    ingredient=ingredient_data.pop('id'),
-                    amount=ingredient_data.pop('amount'),
-                    recipe=recipe,
-                )
-            )
-        IngredientRecipe.objects.bulk_create(ingredient_liist)
+    def create_ingredients(self, ingredients, recipe):
+        IngredientRecipe.objects.bulk_create(
+            [IngredientRecipe(
+                recipe=recipe,
+                ingredient_id=ingredient.get('ingredient').get('id'),
+                amount=ingredient.get('amount')
+            ) for ingredient in ingredients]
+        )
 
     def create(self, validated_data):
         request = self.context.get('request', None)
