4b7510fb18da54f617fb8921d3e40815e3d22888
sukhartsev1987 <138875959+sukhartsev1987@users.noreply.github.com>
2023-07-22 19:44:47 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
 
 
 DJOSER = {
-    'LOGIN_FIELD': 'email',
+    'LOGIN_FIELD': 'email' or 'username',
     'SERIALIZERS': {
         'user_create': 'api.serializers.CustomUserCreateSerializer',
         'user': 'api.serializers.CustomUserSerializer',
